2f10f05cc1d4823d259970d8884322ac65fa5e74
tribagus <tribagus.indonesia@gmail.com>
2023-12-22 10:56:37 +1100

    deploy js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,4 +1,14 @@
-async function main() {}
+const { ethers } = require("hardhat");
+
+async function main() {
+  // fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log("token deployed to:", token.address);
+
+  // deploy the contract
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
