55909694955d191cd474f3d7c05051569ec9a96e
tribagus <tribagus.indonesia@gmail.com>
2024-02-07 14:48:09 +1100

    feat: new contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -14,13 +14,62 @@ contract Token {
 
     // track balances
     mapping(address => uint256) public balanceOf;
+    // for the owner (caller) and the sender (exchanger)
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
     // send tokens
-    constructor (string memory _name, string memory _symbol, uint256 _totalSupply) {
-      name = _name;
-      symbol = _symbol;
-      totalSupply = _totalSupply * (10**decimals); // 1,000,000 X 10^8;
-      balanceOf[msg.sender] = totalSupply;
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10 ** decimals); // 1,000,000 X 10^8;
+        balanceOf[msg.sender] = totalSupply;
     }
 
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // require that sender has enough tokens to spend
+        require(balanceOf[msg.sender] >= _value);
+        // require that sender has correct address
+        require(_to != address(0));
+
+        // deduct tokens from sender
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+
+        // credit tokens to receiver
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        // emit event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
+        // require that spender has correct address
+        require(_spender != address(0));
+
+        allowance[msg.sender][_spender] = _value;
+
+        // emit event
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,21 +1,25 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+// ===== just some variables =====
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
+// ===== the test =====
 describe("Token", () => {
-  let token, accounts, deployer;
+  let token, accounts, deployer, receiver, exchange;
 
   beforeEach(async () => {
     // fetch the token from blockchain
     const Token = await ethers.getContractFactory("Token");
     // deploy to the test
-    token = await Token.deploy("Sholjum", "SHOL", 1000000);
+    token = await Token.deploy("Sholjum", "SHOL", "1000000");
     // fetch the accounts
     accounts = await ethers.getSigners();
     deployer = accounts[0];
+    receiver = accounts[1];
+    exchange = accounts[2];
   });
 
   describe("Deployment", () => {
@@ -47,6 +51,99 @@ describe("Token", () => {
     it("assigns total supply to deployer", async () => {
       // eslint-disable-next-line jest/valid-expect
       expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
+    });
+  });
+
+  describe("Sending tokens", () => {
+    let amount, transaction, result;
+
+    describe("Success", () => {
+      beforeEach(async () => {
+        amount = tokens(100);
+        transaction = await token
+          .connect(deployer)
+          .transfer(receiver.address, amount);
+        // eslint-disable-next-line testing-library/await-async-utils
+        result = await transaction.wait();
+      });
+
+      it("transfers token balances", async () => {
+        // ensure that tokens were transfered (balance changed)
+        expect(await token.balanceOf(deployer.address)).to.equal(
+          tokens(999900)
+        );
+        // eslint-disable-next-line jest/valid-expect
+        expect(await token.balanceOf(receiver.address)).to.equal(amount);
+      });
+
+      it("emits a Transfer event", async () => {
+        const event = result.events[0];
+        expect(event.event).to.equal("Transfer");
+
+        const args = event.args;
+        expect(args.from).to.equal(deployer.address);
+        expect(args.to).to.equal(receiver.address);
+        expect(args.value).to.equal(amount);
+      });
+    });
+
+    describe("Failure", () => {
+      it("rejects insufficient balances", async () => {
+        //  transfer more token than deployer has (100m)
+        const invalidAmount = tokens(100000000);
+        await expect(
+          token.connect(deployer).transfer(receiver.address, invalidAmount)
+        ).to.be.reverted;
+      });
+
+      it("rejects invalid recipient", async () => {
+        const amount = tokens(100);
+        await expect(
+          token
+            .connect(deployer)
+            .transfer("0x0000000000000000000000000000000000000000", amount)
+        ).to.be.reverted;
+      });
+    });
+
+    describe("Approving Tokens", () => {
+      let amount, transaction, result;
+
+      beforeEach(async () => {
+        amount = tokens(100);
+        transaction = await token
+          .connect(deployer)
+          .approve(exchange.address, amount);
+        result = await transaction.wait();
+      });
+
+      describe("Success", () => {
+        it("allocates an allowance for delegated token spending", async () => {
+          expect(
+            await token.allowance(deployer.address, exchange.address)
+          ).to.equal(amount);
+        });
+
+        it("emits an approval event", async () => {
+          const event = result.events[0];
+          expect(event.event).to.equal("Approval");
+
+          const args = event.args;
+          expect(args.owner).to.equal(deployer.address);
+          expect(args.spender).to.equal(exchange.address);
+          expect(args.value).to.equal(amount);
+        });
+      });
+
+      describe("Failure", () => {
+        it("rejects invalid spenders", async () => {
+          await expect(
+            token
+              .connect(deployer)
+              .approve("0x0000000000000000000000000000000000000000", amount)
+          ).to.be.reverted;
+        });
+      });
     });
   });
 });
